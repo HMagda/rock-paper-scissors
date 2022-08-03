53c7a4ee354241fe33e05813765e291a998fe1d2
Magdalena <magdalenahyra@wp.pl>
2022-08-03 22:29:05 +0200

    Add logic and display choose and result

--- js/functions.js
+++
@@ -1,9 +0,0 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
--- js/script.js
+++ js/script.js
@@ -1,21 +1,57 @@
-var computerMove;
-computerMove = 'kamień';
+// const btn = document.createElement("button");
+// btn.innerHTML = "Hello Button";
+// document.body.appendChild(btn);
 
-var playerMove;
-playerMove = 'papier';
+// console.log('Hello World');
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+const userChoiceDisplay = document.createElement('h1');
+const computerChoiceDisplay = document.createElement('h1');
+const resultDisplay = document.createElement('h1');
+const gameGrid = document.getElementById('game');
+gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay);
 
-var computerMove, randomNumber;
-var minVal = 1;
-var maxVal = 3;
-randomNumber = Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal;
-printMessage('Wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else if (randomNumber == '2') {
-    computerMove = 'papier';
-} else {
-  computerMove = 'nożyce';
+const choices = ['rock', 'paper', 'scissors'];
+let userChoice;
+let computerChoice;
+
+const generateComputerChoice = () => {
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+  computerChoice = randomChoice;
+  computerChoiceDisplay.innerHTML = 'Computer choice: ' + computerChoice
+};
+
+const handleClick = (e) => {
+  console.log('clicked');
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = 'User choice: ' + userChoice;
+  generateComputerChoice();
+  getResult();
+};
+
+for (let i = 0; i < choices.length; i++) {
+  const button = document.createElement('button');
+  button.id = choices[i];
+  button.innerHTML = choices[i];
+  button.addEventListener('click', handleClick);
+  gameGrid.appendChild(button)
+};
+
+const getResult = () => {
+  switch (userChoice + computerChoice) {
+    case 'scissorspaper':
+    case 'rockscissors':
+    case 'paperrock':
+      resultDisplay.innerHTML = 'You win!';
+      break;
+    case 'paperscissors':
+    case 'scissorsrock':
+    case 'rockpaper':
+      resultDisplay.innerHTML = 'You lose!';
+      break;
+    case 'paperpaper':
+    case 'scissorsscissors':
+    case 'rockrock':
+      resultDisplay.innerHTML = 'It is a draw!';
+      break;
+  }
 }
-printMessage('Mój ruch: ' + computerMove);

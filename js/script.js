f80d1bb7329251b5ab6262f3b596e9786e22373d
Magdalena <magdalenahyra@wp.pl>
2023-02-23 00:28:57 +0100

    Added TypeScript and edited script.js file

--- js/script.js
+++ js/script.js
@@ -1,52 +1,47 @@
+"use strict";
 const userChoiceDisplay = document.createElement('h1');
 const computerChoiceDisplay = document.createElement('h1');
 const resultDisplay = document.createElement('h1');
 const gameGrid = document.getElementById('game');
 gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay);
 const buttonsWrapper = document.getElementById('buttons');
-
 const choices = ['rock', 'paper', 'scissors'];
 let userChoice;
 let computerChoice;
-
 const generateComputerChoice = () => {
-  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-  return randomChoice;
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
 };
-
 const handleClick = (e) => {
-  console.log('clicked');
-  userChoice = e.target.id;
-  userChoiceDisplay.innerHTML = 'You: ' + userChoice;
-  computerChoice = generateComputerChoice();
-  computerChoiceDisplay.innerHTML = 'Computer: ' + computerChoice;
-  determineAndShowResult();
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = 'You: ' + userChoice;
+    computerChoice = generateComputerChoice();
+    computerChoiceDisplay.innerHTML = 'Computer: ' + computerChoice;
+    determineAndShowResult();
 };
-
 for (let i = 0; i < choices.length; i++) {
-  const button = document.createElement('button');
-  button.id = choices[i];
-  button.innerHTML = choices[i];
-  button.addEventListener('click', handleClick);
-  buttonsWrapper.appendChild(button)
-};
-
-const determineAndShowResult = () => {
-  switch (userChoice + computerChoice) {
-    case 'scissorspaper':
-    case 'rockscissors':
-    case 'paperrock':
-      resultDisplay.innerHTML = 'You WIN!';
-      break;
-    case 'paperscissors':
-    case 'scissorsrock':
-    case 'rockpaper':
-      resultDisplay.innerHTML = 'You LOSE!';
-      break;
-    case 'paperpaper':
-    case 'scissorsscissors':
-    case 'rockrock':
-      resultDisplay.innerHTML = 'It is a DRAW!';
-      break;
-  }
+    const button = document.createElement('button');
+    button.id = choices[i];
+    button.innerHTML = choices[i];
+    button.addEventListener('click', handleClick);
+    buttonsWrapper.appendChild(button);
 }
+const determineAndShowResult = () => {
+    switch (userChoice + computerChoice) {
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+            resultDisplay.innerHTML = 'You WIN!';
+            break;
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            resultDisplay.innerHTML = 'You LOSE!';
+            break;
+        case 'paperpaper':
+        case 'scissorsscissors':
+        case 'rockrock':
+            resultDisplay.innerHTML = 'It is a DRAW!';
+            break;
+    }
+};
---
+++ js/script.ts
@@ -0,0 +1,52 @@
+const userChoiceDisplay: HTMLHeadingElement = document.createElement('h1');
+const computerChoiceDisplay: HTMLHeadingElement = document.createElement('h1');
+const resultDisplay: HTMLHeadingElement = document.createElement('h1');
+const gameGrid: HTMLHeadingElement = document.getElementById('game')!;
+gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay);
+const buttonsWrapper = document.getElementById('buttons')!;
+
+const choices: string[] = ['rock', 'paper', 'scissors'];
+let userChoice: string;
+let computerChoice: string;
+
+const generateComputerChoice = (): string => {
+  const randomChoice: string = choices[Math.floor(Math.random() * choices.length)];
+  return randomChoice;
+};
+
+const handleClick = (e: MouseEvent): void => {
+  userChoice = (e.target as HTMLElement).id;
+
+  userChoiceDisplay.innerHTML = 'You: ' + userChoice;
+  computerChoice = generateComputerChoice();
+  computerChoiceDisplay.innerHTML = 'Computer: ' + computerChoice;
+  determineAndShowResult();
+};
+
+for (let i = 0; i < choices.length; i++) {
+  const button = document.createElement('button');
+  button.id = choices[i];
+  button.innerHTML = choices[i];
+  button.addEventListener('click', handleClick);
+  buttonsWrapper.appendChild(button);
+}
+
+const determineAndShowResult = () => {
+  switch (userChoice + computerChoice) {
+    case 'scissorspaper':
+    case 'rockscissors':
+    case 'paperrock':
+      resultDisplay.innerHTML = 'You WIN!';
+      break;
+    case 'paperscissors':
+    case 'scissorsrock':
+    case 'rockpaper':
+      resultDisplay.innerHTML = 'You LOSE!';
+      break;
+    case 'paperpaper':
+    case 'scissorsscissors':
+    case 'rockrock':
+      resultDisplay.innerHTML = 'It is a DRAW!';
+      break;
+  }
+};
